1b5595d7da8e7a18fb7be2baef8b7c2c75109d8a
Tuanle207 <tuan.leanh@elca.vn>
2022-01-20 11:19:29 +0700

    Add UI for Projects List Page

--- src/app/pages/project-management/project-management.component.ts
+++ src/app/pages/project-management/project-management.component.ts
@@ -1,11 +1,25 @@
+import { ChangeDetectionStrategy } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { PaginationInstance } from 'ngx-pagination';
 
 @Component({
   selector: 'app-project-management',
   templateUrl: './project-management.component.html',
-  styleUrls: ['./project-management.component.scss']
+  styleUrls: ['./project-management.component.scss'],
 })
 export class ProjectManagementComponent implements OnInit {
+
+  items = Array.from(new Array(100)).map((_, index) => {
+    return index;
+  });
+
+  page = 1;
+
+  paginationConfig: PaginationInstance = {
+    id: 'project-pagination',
+    itemsPerPage: 10,
+    currentPage: 1
+  } 
 
   constructor() { }
 
--- src/app/pages/project-management/project-management.module.ts
+++ src/app/pages/project-management/project-management.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 import { ProjectManagementComponent } from './project-management.component';
 import { ProjectManagementRoutingModule } from './project-management-routing.module';
 
 
-
 @NgModule({
   declarations: [
-    ProjectManagementComponent
+    ProjectManagementComponent,
   ],
   imports: [
     ProjectManagementRoutingModule,
+    NgxPaginationModule,
     CommonModule,
   ]
 })
